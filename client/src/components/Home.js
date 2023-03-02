d70b5ef294a110f4e768c6c0bcf873e2f7d4f2f1
Kejuan Harrison <bamaboi912@icloud.com>
2023-03-02 16:13:14 -0600

    added styles.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import { Button, Typography, Box } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";.
-import
+
 const Home = () => {
   return (
     <div>
